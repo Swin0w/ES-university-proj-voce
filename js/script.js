528ca6ec18ff016ea24979a8f90f5e5c2b156a24
Simone <marzolla.istruzione@gmail.com>
2021-11-22 20:28:11 +0100

    Upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function init() {
+    new Vue({
+        el: '#app',
+        data: {
+            title: [
+                "Give voice to your brand",
+                "Grow up togheter",
+            ]
+        }
+    });
+}
+document.addEventListener('DOMContentLoaded', init);
